931c1c32b79fd84b2861d15e3ed5c1139a49058c
arvindpazare <arvind.pazare@gmail.com>
2017-05-31 10:24:25 -0400

    Added missing test case

--- src/test/java/com/photo/album/photoAlbum/runner/ApplicationStarterTest.java
+++ src/test/java/com/photo/album/photoAlbum/runner/ApplicationStarterTest.java
@@ -53,6 +53,17 @@ public class ApplicationStarterTest extends TestCase {
         Mockito.verify(exitApplication, atLeastOnce()).exit(0);
     }
 
+    /*
+    @Test
+    public void testShouldNotProcessOrQuitOnInvalidInput() throws Exception {
+        when(photoAlbumConsole.isSystemConsolePresent()).thenReturn(false);
+        when(photoAlbumConsole.readFromConsole()).thenReturn("3x");
+        applicationStarter.start();
+        Mockito.verify(exitApplication, never()).exit(0);
+        Mockito.verify(photoAlbumServiceIfc, never()).getPhotoAlbumById(anyLong());
+    }
+    */
+    
     @Before
     public void initMocks() {
         MockitoAnnotations.initMocks(this);
